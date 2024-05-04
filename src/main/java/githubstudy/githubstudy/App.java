f3b8eab3843f062e8dbfe0157935380e8a158ef5
Tanishka <bist.tanishka@gmail.com>
2024-05-04 22:06:54 +0530

    first commit

---
+++ src/main/java/githubstudy/githubstudy/App.java
@@ -0,0 +1,13 @@
+package githubstudy.githubstudy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/githubstudy/githubstudy/Sample.java
@@ -0,0 +1,7 @@
+package githubstudy.githubstudy;
+
+public class Sample {
+public static void main(String[] args) {
+	System.out.println("Sample.main()");
+}
+}
