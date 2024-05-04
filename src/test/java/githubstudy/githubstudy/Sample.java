b6dd01842fd5b87ab3207f8b5297b6552561786d
Tanishka <bist.tanishka@gmail.com>
2024-05-04 22:19:44 +0530

    SubBranch2 added

--- src/test/java/githubstudy/githubstudy/Sample.java
+++ src/test/java/githubstudy/githubstudy/Sample.java
@@ -3,5 +3,6 @@ package githubstudy.githubstudy;
 public class Sample {
 public static void main(String[] args) {
 	System.out.println("Sample.main()");
+	System.out.println("SUbBracnh2 added");
 }
 }
